06fd43b51f9f019e0d17e454a08711eb9f4017c0
oweissbarth <mail@oweissbarth.de>
2013-05-19 16:13:10 +0200

    improved Start screen layout > Added check for existing Projects

--- src/de/oweissbarth/slate/IntroActivity.java
+++ src/de/oweissbarth/slate/IntroActivity.java
@@ -3,8 +3,9 @@ package de.oweissbarth.slate;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class IntroActivity extends Activity {
 
@@ -12,6 +13,21 @@ public class IntroActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro);
+		
+		
+		String[] availableFiles = File.listProjects();
+		ArrayAdapter<String> adapter = null;
+		if (availableFiles.length!=0){
+			
+			
+			adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, availableFiles);
+			
+		}else{
+			adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, new String[]{"No Projects"});
+		}
+		Spinner projects = (Spinner) findViewById(R.id.projectsSpinner);
+		projects.setAdapter(adapter);
+		
 	}
 
 	public void newProject(View view){
