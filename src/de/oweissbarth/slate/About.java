a8d47d09f7e3b9327962d333016ee5d88bc5f30f
oweissbarth <mail@oweissbarth.de>
2013-09-17 17:58:15 +0200

    Added About Page

--- src/de/oweissbarth/slate/About.java
+++ src/de/oweissbarth/slate/About.java
@@ -1,9 +1,8 @@
 package de.oweissbarth.slate;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.view.Menu;
+import android.os.Bundle;
 import android.widget.TextView;
 
 public class About extends Activity {
@@ -13,6 +12,7 @@ public class About extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 		try {
+			//try to get app version
 			((TextView)findViewById(R.id.about_version)).setText("version " + this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName);
 		} catch (NameNotFoundException e) {
 			((TextView)findViewById(R.id.about_version)).setText("couldn't load App version");
